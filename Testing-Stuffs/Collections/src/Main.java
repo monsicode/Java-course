c82d59514f82319188568500a4237fa5d62a36bc
monica pencheva <monikapeneva02@gmail.com>
2024-11-02 11:24:35 +0200

    lab_4 created

---
+++ Testing-Stuffs/Collections/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(4);
+        
+    }
+
+
+
+
+
+}
